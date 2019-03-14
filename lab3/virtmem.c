a592446537f6afcab40237f8b17772431254a3d0
Demiurg-in-real <bezginandrew@gmail.com>
2019-03-14 09:31:34 -0400

    HZ

--- lab3/virtmem.c
+++ lab3/virtmem.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	sd=open(argv[1], O_RDONLY);
 	fstat(sd, &st);
 	ptr=(char*)mmap(NULL,st.st_size, PROT_READ, MAP_PRIVATE,sd,0); //pochitai pro virtualnuu pamat, a to tak ne sovsem ponyatno s hodu... Mdeeee... Ti tupoi, Andrei
-	printf("%04x\n", *((unsigned short*)(ptr+510+2*2)));
+	printf("%04x\n", *((unsigned short*)(ptr+510)));
 	munmap(ptr, st.st_size);
 	close(sd);
 	return 0;
