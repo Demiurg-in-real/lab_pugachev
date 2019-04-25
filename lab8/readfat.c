53dd7287747e660ad2e1d857d19e77c66cc73b22
Demiurg-in-real <bezginandrew@gmail.com>
2019-04-25 12:13:58 -0400

    Почти

--- lab8/readfat.c
+++ lab8/readfat.c
@@ -1,4 +1,5 @@
 #include<fcntl.h>
+#include<string.h>
 #include<unistd.h>
 #include<stdio.h>
 #include<sys/types.h>
@@ -63,13 +64,13 @@ struct katalog {
 }__attribute__((packed));
 struct katalog_long_name{
 	uint8_t number_of_fragment;
-	uint8_t first_name[10];
+	char first_name[10];
 	uint8_t file_attribute;
 	uint8_t byte_flag;
 	uint8_t kontrol_sum;
-	uint32_t second_name[3];
+	char second_name[12];
 	uint16_t numb_first_klust; //must be zero 0
-	uint32_t third_name; //0x1c
+	char third_name[4]; //0x1c
 }__attribute__((packed));
 struct FSInfo {
 	uint32_t signature;
@@ -91,13 +92,15 @@ struct table_fat32 {
 }__attribute__((packed));
 
 
+char* get_file(char* name, uint32_t from, uint32_t razmer, int fi);
+
 int main(int argc, char* argv[])
 {
 	uint8_t *ptr; 
 	int file_read;
 	uint32_t go_to;
 	struct stat statistic;
-	struct fat1216 boot_sector; 
+	struct fat1216 boot_sector;
 	file_read=open(argv[1], O_RDONLY);
 	if (file_read == -1){
 		printf("Some troubles with file. It can be:\n1)Program doesn't have the required permissions.\n2)You didn't specify the first parametr.\n3)You are bitch!\n");
@@ -107,33 +110,99 @@ int main(int argc, char* argv[])
 	ptr=mmap(NULL, statistic.st_size, PROT_READ, MAP_PRIVATE,file_read, 0);
 	boot_sector=*((struct fat1216*)(ptr+0));
 	//printf("%04x\n", boot_sector.OEM);
-	go_to=boot_sector.size_sector*(/*boot_sector.num_sec_in_klust*/boot_sector.res_num_sec_in_klust+boot_sector.num_tables*boot_sector.num_sect)+boot_sector.num_files_in_korn*32;
-	printf("%04x\n",go_to);
+	go_to=boot_sector.size_sector*(/*boot_sector.num_sec_in_klust*/boot_sector.res_num_sec_in_klust+boot_sector.num_tables*boot_sector.num_sect);//+boot_sector.num_files_in_korn*32;
+	//printf("%04x\n",go_to);
+	int y=0, z=0;
 	struct katalog katal[boot_sector.num_files_in_korn];
 	struct katalog_long_name kata[boot_sector.num_files_in_korn];
+	struct katalog add_kat[16][16];
+	struct katalog_long_name add_kata[16][16];
 	for (int i=0; i<=boot_sector.num_files_in_korn*32;i+=32){
 		katal[i/32]=*((struct katalog*)(ptr+go_to+i));
 		if (katal[i/32].attributes_of_file == 0x0f){
 			kata[i/32]=*((struct katalog_long_name*)(ptr+go_to+i));
 		}
 		if (katal[i/32].attributes_of_file == 0x10){
-			//attributes
+			do{
+			add_kat[z][y]=*((struct katalog*)(ptr+go_to+boot_sector.num_files_in_korn*32+(katal[i/32].low_word_first_klust-2)*boot_sector.size_sector));
+			if(add_kat[z][y].attributes_of_file==0x0f){
+				add_kata[z][y]=*((struct katalog_long_name*)(ptr+go_to+boot_sector.num_files_in_korn*32+(katal[i/32].low_word_first_klust-2)*boot_sector.size_sector));
+			}
+			y++;
+			//go_to=go_to+boot_sector.num_files_in_korn*32+(katal[i/32].low_word_first_klust-2)*boot_sector.size_sector;
+			//printf("Yep %08x\n", go_to);//add_kat[z][y].low_word_first_klust);
+			}
+			while(add_kat[z][y].low_word_first_klust!=0x0);
+			z++;
+		}
+		//printf("%08x\n", katal[i/32].low_word_first_klust);
+
+	}
+	//printf("3\n");
+	// adding attributes for reading file
+	char* Name="zerp";
+	uint16_t offset, size=boot_sector.size_sector*boot_sector.num_sec_in_klust;
+	go_to=go_to+boot_sector.num_files_in_korn*32;
+	uint32_t where;
+	char *simv;
+	//writing files
+	for(int i=0; i<boot_sector.num_files_in_korn; i++)
+	{
+		fIle:
+		if (katal[i].attributes_of_file == 0x10){
+			//
+		}
+		else
+		{
+			//printf("4\n");
+			if(katal[i].attributes_of_file == 0x0f)
+			{
+				for(int d=1; d<11; d++)
+				{
+					printf("hey\n");
+					*simv=kata[i].first_name[d];
+					strcat(Name,simv);//kata[i].first_name[d]);
+					printf("hi!\n");
+				}
+				for(int d=0; d<3*4;d++)
+				{
+					strcat(Name, kata[i].second_name[d]);
+				}
+				for(int d=0; d<4; d++){
+					strcat(Name,kata[i].third_name);
+				}
+			}
+			if(katal[i].attributes_of_file!=0x0f && katal[i].attributes_of_file!=0x10 && katal[i].short_name[1]!=0x00){
+					offset=katal[i].low_word_first_klust-2;
+					where=offset*size+go_to;
+					printf("%i\n", i);
+					printf("%08x\n", offset);
+					Name=get_file(Name,where, katal[i].size, file_read);
+				}
+			
 		}
 	}
 	return 0;	
 }
 
-/*void get_file()
+char* get_file(char* name, uint32_t from, uint32_t razmer, int fi)
 {
-	uint8_t *simv;
+	uint8_t *simv, *ptr;
+	struct stat get;
+	fstat(fi, &get);
+	ptr=(uint8_t*)mmap(NULL, get.st_size, PROT_READ, MAP_PRIVATE,fi,0);
 	FILE *writ=fopen(name, "wb");
-	if(writ==NULL) break;
+	if(writ==NULL) return 0;
+	printf("%08x\n", from);
 	for(int i=0; i<razmer;i++)
 	{
-		simv=*((uint8_t*)(ptr+i+smesh+klust_begin));
+		simv=*((uint8_t*)(ptr+i+from));
 		fwrite(&simv, sizeof(uint8_t),1,writ);
 	}
+	printf("1\n");
 	fclose(writ);
+	name="";
+	return name;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /*
